d9365aaa5e8b019de0612578f5dc8b55865c64d5
Mateusz <mateusz.danieluk@ge.com>
2019-03-29 12:29:06 +0100

    Fixed activity query

--- queries.js
+++ queries.js
@@ -21,7 +21,7 @@ const getActors = (req, res) => getData(req,res,'SELECT * from public.actor')
 
 const getPgStatStatements = (req,res)=> getData(req,res,'SELECT query,calls,min_time,max_time,mean_time,rows FROM pg_stat_statements ORDER BY calls DESC')
 
-const getPgStatActivity = (req,res)=> getData(req,res,'SELECT pid,usesysid,application_name,client_addr, client_port,state,query FROM pg_stat_statements')
+const getPgStatActivity = (req,res)=> getData(req,res,`SELECT pid,usesysid,application_name,client_addr, client_port,state,query FROM pg_stat_activity where datname = 'mateusz'`)
 
 
   module.exports = {
